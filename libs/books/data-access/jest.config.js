27cae35f0b7e5f43683ad4a56d70764248afd763
cmounika <cmounika9854@gmail.com>
2024-10-16 12:05:54 +0530

    chore: add code review and fix tests

--- libs/books/data-access/jest.config.js
+++ libs/books/data-access/jest.config.js
@@ -1,21 +1,25 @@
+const ANGULAR_SNAPSHOT_SERIALIZERS = [
+  'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+  'jest-preset-angular/build/AngularSnapshotSerializer.js',
+  'jest-preset-angular/build/HTMLCommentSerializer.js',
+];
+
+const TS_JEST_TRANSFORMERS = [
+  'jest-preset-angular/build/InlineFilesTransformer',
+  'jest-preset-angular/build/StripStylesTransformer',
+];
+
 module.exports = {
   name: 'books-data-access',
   preset: '../../../jest.config.js',
   coverageDirectory: '../../../coverage/libs/books/data-access',
-  snapshotSerializers: [
-    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
-    'jest-preset-angular/build/AngularSnapshotSerializer.js',
-    'jest-preset-angular/build/HTMLCommentSerializer.js',
-  ],
+  snapshotSerializers: ANGULAR_SNAPSHOT_SERIALIZERS,
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   globals: {
     'ts-jest': {
       tsConfig: '<rootDir>/tsconfig.spec.json',
       stringifyContentPathRegex: '\\.(html|svg)$',
-      astTransformers: [
-        'jest-preset-angular/build/InlineFilesTransformer',
-        'jest-preset-angular/build/StripStylesTransformer',
-      ],
+      astTransformers: TS_JEST_TRANSFORMERS,
     },
   },
 };
